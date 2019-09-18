06a9716e9be69f7b7ad3c5216342af1fd945edb0
Roger-Wu <gsetcrw@gmail.com>
2019-09-18 19:16:05 +0800

    init

---
+++ precache-manifest.ac592bbb36ea607afa277cdf3d41b98a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9162439746dd66bae97e3255cd2bfd5",
+    "url": "./static/media/selfcoin.c9162439.png"
+  },
+  {
+    "revision": "7fde2a43c5df891bb576b23b6a96fefe",
+    "url": "./static/media/bg_line.7fde2a43.png"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "356f6a34e46db6def399",
+    "url": "./static/js/main.356f6a34.chunk.js"
+  },
+  {
+    "revision": "2a9de652e86ad3f3a5d3",
+    "url": "./static/js/1.2a9de652.chunk.js"
+  },
+  {
+    "revision": "356f6a34e46db6def399",
+    "url": "./static/css/main.3f73410a.chunk.css"
+  },
+  {
+    "revision": "500b9d4e490d86c26e4831660e0ea0ba",
+    "url": "./index.html"
+  }
+];
